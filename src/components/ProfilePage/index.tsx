e1f733c7b72e27c3e267822bbf48117781ed4cb4
paulordn <paulo11.rondon@gmail.com>
2021-09-27 13:51:17 -0300

    Projeto

---
+++ src/components/ProfilePage/index.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+import Feed from '../Feed';
+
+import {
+  Container,
+  Banner,
+  Avatar,
+  ProfileData,
+  LocationIcon,
+  CakeIcon,
+  Followage,
+  EditButton,
+} from './styles';
+
+const ProfilePage: React.FC = () => {
+  return (
+    <Container>
+      <Banner>
+        <Avatar />
+      </Banner>
+
+      <ProfileData>
+        <EditButton outlined>Editar perfil</EditButton>
+
+        <h1>Paulo Rondon</h1>
+        <h2>@Paulo11_rondon</h2>
+
+        <p>
+          Developer F/A <a href="www.google.com">Google</a>
+        </p>
+
+        <ul>
+          <li>
+            <LocationIcon />
+            São Paulo, Brasil
+          </li>
+          <li>
+            <CakeIcon />
+            Nascido(a) em 18 de fevereiro 
+          </li>
+        </ul>
+
+        <Followage>
+          <span>
+            seguindo <strong>94</strong>
+          </span>
+          <span>
+            <strong>672 </strong> seguidores
+          </span>
+        </Followage>
+      </ProfileData>
+
+      <Feed />
+    </Container>
+  );
+};
+
+export default ProfilePage;
